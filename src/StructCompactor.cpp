3664468d0b46e0f60d23f91e0294e61e35ba7a8c
Hugo Leclerc <leclerc@lmt.ens-cachan.fr>
2010-10-07 15:30:42 +0200

    refonte StructCompactor

--- src/StructCompactor.cpp
+++ src/StructCompactor.cpp
@@ -2,78 +2,28 @@
 
 BEG_METIL_NAMESPACE;
 
-StructCompactor *StructCompactor::base_str() {
-    if ( pointed_type )
-        return pointed_type->base_str();
-    return this;
+void StructCompactor::ItemSca::make_decl( String &os, const String &sp ) {
 }
 
-String StructCompactor::needed_alignement() const {
-    if ( type == "FP32" or type == "FP64" or type == "SI32" or type == "SI64" )
-        return "16 * 4";
-    return "sizeof( ST )";
-}
-
-bool StructCompactor::is_POD() const {
-    for( int i = 0; i < items.size(); ++i )
-        if ( not items[ i ]->is_POD() )
-            return 0;
-    return pointed_type == 0;
-}
-
-void StructCompactor::make_copy( String &os, const String &sp, const String &dst, const String &src, int par_lev ) {
-    for( int i = 0; i < items.size(); ++i ) {
-        os << sp << "// " << items[ i ]->name << " \n";
-        if ( items[ i ]->pointed_type ) {
-            String size; size << src << '.' << items[ i ]->name << ".size()";
-            String ptr ; ptr  << src << '.' << items[ i ]->name << ".ptr()";
-            os << sp << dst << '.' << items[ i ]->name << ".size_ = " << size << ";\n";
-            os << sp << dst << '.' << items[ i ]->name << ".rese_ = " << size << ";\n";
-            if ( items[ i ]->pointed_type->is_POD() ) { // Vec<POD> (e.g. Vec<FP64>)
-                os << sp << "md.copy( &" << dst << '.' << items[ i ]->name << ".data_, " << ptr << ", " << size << " * sizeof( " << items[ i ]->pointed_type->type << " ), " << items[ i ]->pointed_type->needed_alignement() << " );\n";
-            } else if ( items[ i ]->pointed_type->int_type ) { // Vec<LocalStruct>
-                os << sp << dst << '.' << items[ i ]->name << ".data_ = " << items[ i ]->pointed_type->type << "::push_copy_in( md, " << ptr << ", " << size << " );\n";
-            } else { // Vec<Vec<...> >
-                items[ i ]->pointed_type->make_push( os, sp, items[ i ]->name + ".data_", par_lev + 1, '_' + String( par_lev ), size );
-            }
-        } else {
-            if ( items[ i ]->int_type ) // recursive copy
-                items[ i ]->make_copy( os, sp, dst + '.' + items[ i ]->name, src + '.' + items[ i ]->name, par_lev );
-            else // simple attibute
-                os << sp << dst << '.' << items[ i ]->name << " = " << src << '.' << items[ i ]->name << ";\n";
-        }
-    }
-}
-
-void StructCompactor::make_push( String &os, const String &sp, const String &dst, int par_lev, const String &suf, const String &n ) {
-    os << sp << type << " *loc" << suf << "; ST rese" << suf << " = " << n << " * sizeof( " << type << " );\n";
-    os << sp << "md.beg_local_data( &" << dst << ", &loc" << suf << ", rese" << suf << ", " << needed_alignement() << " );\n";
-    String i = char( 'i' + par_lev );
-    os << sp << "for( ST " << i << " = 0; " << i << " < n; ++" << i << " ) {\n";
-    make_copy( os, sp + "    ", "loc" + suf + "[ " + i + " ]", "src[ " + i + " ]", par_lev );
-    os << sp << "}\n";
-    os << sp << "md.end_local_data( " << dst << ", loc" << suf << ", rese" << suf << " );\n";
-}
-
-void StructCompactor::make_decl( String &os, const String &sp ) {
-    os << sp << "struct " << type << " {\n";
+void StructCompactor::ItemStr::make_decl( String &os, const String &sp ) {
     String np = sp + "    ";
     String op = np + "    ";
+    os << sp << "struct " << type << " {\n";
 
-    // types
+    // declarations
     BasicVec<String> decl_types;
     for( int i = 0; i < items.size(); ++i ) {
-        StructCompactor *bs = items[ i ]->base_str();
-        if ( bs->int_type and not decl_types.contains( bs->type ) ) {
-            decl_types << bs->type;
+        Item *bs = items[ i ]->base_str();
+        if ( not decl_types.contains( bs->type ) ) {
             bs->make_decl( os, np );
+            decl_types << bs->type;
         }
     }
 
-    // copy
-    os << np << "template<class T> static " << type << " *push_copy_in( MemoryDriver &md, const T *src, ST n = 1 ) {\n";
+    // methods
+    os << np << "template<class T> static " << type << " *copy( MemoryDriver &md, const T *src, ST num = 1 ) {\n";
     os << np << "    " << type << " *dst;\n";
-    make_push( os, op, "dst" );
+    make_copy( os, op, "dst", "src", "num", 0 );
     os << np << "    return dst;\n";
     os << np << "}\n";
 
@@ -84,30 +34,117 @@ void StructCompactor::make_decl( String &os, const String &sp ) {
     os << sp << "};\n";
 }
 
-void StructCompactor::make_defi( String &os, const String &sp ) {
-    os << "#include <CudaMetil.h>\n";
-    os << "void f() {}\n";
+void StructCompactor::ItemVec::make_decl( String &os, const String &sp ) {
 }
 
-void StructCompactor::make_file() {
-    File fh( type + ".h", "w" );
-    fh << "#ifndef " << type << "_H\n";
-    fh << "#define " << type << "_H\n";
+void StructCompactor::ItemSca::make_copy( String &os, const String &sp, const String &dst, const String &src, const String &num, int par_level ) {
+}
+
+void StructCompactor::ItemStr::make_copy( String &os, const String &sp, const String &dst, const String &src, const String &num, int par_level ) {
+    os << sp << type << " *loc;\n";
+    os << sp << "md.beg_local_data( &" << dst << ", &loc, " << num << " * sizeof( " << type << " ), " << alig() << " );\n";
+    String i = char( 'i' + par_level );
+    os << sp << "for( ST " << i << " = 0; " << i << " < " << num << "; ++" << i << " ) {\n";
+    String vdst; vdst << dst << "[ " << i << " ]";
+    String vsrc; vsrc << src << "[ " << i << " ]";
+    for( int i = 0; i < items.size(); ++i )
+        items[ i ]->copy_attr( os, sp + "    ", vdst, vsrc, par_level + 1 );
+    os << sp << "}\n";
+    os << sp << "md.end_local_data( " << dst << ", loc, " << num << " * sizeof( " << type << " ) );\n";
+}
+
+void StructCompactor::ItemVec::make_copy( String &os, const String &sp, const String &dst, const String &src, const String &num, int par_level ) {
+}
+
+void StructCompactor::ItemSca::copy_attr( String &os, const String &sp, const String &dst, const String &src, int par_level ) {
+    os << sp << dst << '.' << name << " = " << src << '.' << name << ";\n";
+}
+
+void StructCompactor::ItemStr::copy_attr( String &os, const String &sp, const String &dst, const String &src, int par_level ) {
+    String vdst; vdst << dst << '.' << name;
+    String vsrc; vsrc << dst << '.' << name;
+    for( int i = 0; i < items.size(); ++i )
+        items[ i ]->copy_attr( os, sp, vdst, vsrc, par_level );
+}
+
+void StructCompactor::ItemVec::copy_attr( String &os, const String &sp, const String &dst, const String &src, int par_level ) {
+    String size; size << src << '.' << name << ".size()";
+    os << sp << dst << '.' << name << ".size_ = " << size << ";\n";
+    os << sp << dst << '.' << name << ".rese_ = " << size << ";\n";
+    data_type->make_copy( os, sp, dst + '.' + name + ".data_", src + '.' + name + ".ptr()", size, par_level );
+
+}
+
+void StructCompactor::make_files() {
+    File fh( item->type + ".h", "w" );
+    fh << "#ifndef " << item->type << "_H\n";
+    fh << "#define " << item->type << "_H\n";
     fh << "\n";
     fh << "#include <MemoryDriver.h>\n";
     fh << "#include <BasicVecRef.h>\n";
     fh << "\n";
     fh << "BEG_METIL_NAMESPACE\n";
     fh << "\n";
-    make_decl( fh );
+    item->make_decl( fh );
     fh << "\n";
     fh << "END_METIL_NAMESPACE\n";
     fh << "\n";
-    fh << "#endif // " << type << "_H\n";
+    fh << "#endif // " << item->type << "_H\n";
 
-    File fc( type + ".cu", "w" );
-    make_defi( fc );
+    File fc( item->type + ".cu", "w" );
+    // item->make_defi( fc );
+    fc << "#include <CudaMetil.h>\n";
+    fc << "void f() {}\n";
 }
+
+
+//String StructCompactor::needed_alignement() const {
+//    if ( type == "FP32" or type == "FP64" or type == "SI32" or type == "SI64" )
+//        return "16 * 4";
+//    return "sizeof( ST )";
+//}
+
+//bool StructCompactor::is_POD() const {
+//    for( int i = 0; i < items.size(); ++i )
+//        if ( not items[ i ]->is_POD() )
+//            return 0;
+//    return pointed_type == 0;
+//}
+
+//void StructCompactor::make_copy( String &os, const String &sp, const String &dst, const String &src, int par_lev ) {
+//    for( int i = 0; i < items.size(); ++i ) {
+//        os << sp << "// " << items[ i ]->name << " \n";
+//        if ( items[ i ]->pointed_type ) {
+//            String size; size << src << '.' << items[ i ]->name << ".size()";
+//            String ptr ; ptr  << src << '.' << items[ i ]->name << ".ptr()";
+//            os << sp << dst << '.' << items[ i ]->name << ".size_ = " << size << ";\n";
+//            os << sp << dst << '.' << items[ i ]->name << ".rese_ = " << size << ";\n";
+//            if ( items[ i ]->pointed_type->is_POD() ) { // Vec<POD> (e.g. Vec<FP64>)
+//                os << sp << "md.copy( &" << dst << '.' << items[ i ]->name << ".data_, " << ptr << ", " << size << " * sizeof( " << items[ i ]->pointed_type->type << " ), " << items[ i ]->pointed_type->needed_alignement() << " );\n";
+//            } else if ( items[ i ]->pointed_type->int_type ) { // Vec<LocalStruct>
+//                os << sp << dst << '.' << items[ i ]->name << ".data_ = " << items[ i ]->pointed_type->type << "::push_copy_in( md, " << ptr << ", " << size << " );\n";
+//            } else { // Vec<Vec<...> >
+//                items[ i ]->pointed_type->make_push( os, sp, items[ i ]->name + ".data_", par_lev + 1, '_' + String( par_lev ), size );
+//            }
+//        } else {
+//            if ( items[ i ]->int_type ) // recursive copy
+//                items[ i ]->make_copy( os, sp, dst + '.' + items[ i ]->name, src + '.' + items[ i ]->name, par_lev );
+//            else // simple attibute
+//                os << sp << dst << '.' << items[ i ]->name << " = " << src << '.' << items[ i ]->name << ";\n";
+//        }
+//    }
+//}
+
+//void StructCompactor::make_push( String &os, const String &sp, const String &dst, int par_lev, const String &suf, const String &n ) {
+//    os << sp << type << " *loc" << suf << "; ST rese" << suf << " = " << n << " * sizeof( " << type << " );\n";
+//    os << sp << "md.beg_local_data( &" << dst << ", &loc" << suf << ", rese" << suf << ", " << needed_alignement() << " );\n";
+//    String i = char( 'i' + par_lev );
+//    os << sp << "for( ST " << i << " = 0; " << i << " < n; ++" << i << " ) {\n";
+//    make_copy( os, sp + "    ", "loc" + suf + "[ " + i + " ]", "src[ " + i + " ]", par_lev );
+//    os << sp << "}\n";
+//    os << sp << "md.end_local_data( " << dst << ", loc" << suf << ", rese" << suf << " );\n";
+//}
+
 
 
 END_METIL_NAMESPACE;
--- src/StructCompactor.h
+++ src/StructCompactor.h
@@ -16,51 +16,109 @@ public:
     struct BasicProfile {
     };
 
-    /// constructor for generic structure
+    struct Item {
+        NEW_AND_DEL_COMPATIBLE_PURE_VIRTUAL;
+
+        void set_type( const char *t ) { type = t; }
+        virtual void make_decl( String &os, const String &sp = "" ) = 0;
+        virtual Item *base_str() { return this; }
+        virtual void make_copy( String &os, const String &sp, const String &dst, const String &src, const String &num, int par_level ) = 0;
+        virtual void copy_attr( String &os, const String &sp, const String &dst, const String &src, int par_level ) = 0;
+        virtual String alig() const = 0;
+        // bool is_POD() const;
+
+        String type;
+        String name;
+    };
+
+    struct ItemSca : public Item {
+        NEW_AND_DEL_COMPATIBLE;
+
+        ItemSca( const String &type, const String &name ) {
+            this->type = type;
+            this->name = name;
+        }
+
+        virtual void make_decl( String &os, const String &sp = "" );
+        virtual void make_copy( String &os, const String &sp, const String &dst, const String &src, const String &num, int par_level );
+        virtual void copy_attr( String &os, const String &sp, const String &dst, const String &src, int par_level );
+        virtual String alig() const { return "16 * 4"; }
+    };
+
+    struct ItemStr : public Item {
+        NEW_AND_DEL_COMPATIBLE;
+
+        template<class T>
+        ItemStr( const T &inst, const String &name, int par_level ) : par_level( par_level ) {
+            this->name = name;
+            inst.apply_bs( *this, BasicProfile() );
+        }
+
+        template<class T>
+        void apply( const String &name, const T &val ) { items << StructCompactor::new_Item( val, name, par_level ); }
+
+        virtual void make_decl( String &os, const String &sp = "" );
+        virtual void make_copy( String &os, const String &sp, const String &dst, const String &src, const String &num, int par_level );
+        virtual void copy_attr( String &os, const String &sp, const String &dst, const String &src, int par_level );
+        virtual String alig() const { return "sizeof( ST )"; }
+
+        BasicVec<Item *> items;
+        int par_level;
+    };
+
+    struct ItemVec : public Item {
+        NEW_AND_DEL_COMPATIBLE;
+
+        template<class T>
+        ItemVec( const T &inst, const String &name, int par_level ) {
+            this->data_type = StructCompactor::new_Item( inst[ 0 ], name + '[' + char( 'i' + par_level ) + ']', par_level + 1 );
+            this->type << "BasicVecRef<" << data_type->type << " >";
+            this->name = name;
+        }
+
+        virtual void make_decl( String &os, const String &sp = "" );
+        virtual void make_copy( String &os, const String &sp, const String &dst, const String &src, const String &num, int par_level );
+        virtual void copy_attr( String &os, const String &sp, const String &dst, const String &src, int par_level );
+        virtual Item *base_str() { return data_type->base_str(); }
+        virtual String alig() const { return "sizeof( ST )"; }
+
+        Item *data_type;
+    };
+
+
+    // variants of new_Item
     template<class T>
-    StructCompactor( const T &inst, const String &name = "", int par_level = 0 ) : name( name ), pointed_type( 0 ), par_level( par_level ), int_type( 1 ) {
-        inst.apply_bs( *this, BasicProfile() );
+    static Item *new_Item( const T &inst, const String &name, int par_level ) {
+        return NEW( ItemStr, inst, name, par_level );
     }
 
-    /// constructors for POD types
-    #define SC_INIT( T ) \
-        StructCompactor( const T &, const String &name = "", int par_level = 0 ) : type( TypeInformation<T>::type() ), name( name ), pointed_type( 0 ), par_level( par_level ), int_type( 0 ) {}
-    SC_INIT( SI32 );
-    SC_INIT( SI64 );
-    SC_INIT( FP32 );
-    SC_INIT( FP64 );
-    #undef SC_INIT
+    #define NEW_ITEM( T ) \
+        static Item *new_Item( const T &, const String &name, int par_level ) { return NEW( ItemSca, TypeInformation<T>::type(), name ); }
+    NEW_ITEM( SI32 );
+    NEW_ITEM( SI64 );
+    NEW_ITEM( FP32 );
+    NEW_ITEM( FP64 );
+    #undef NEW_ITEM
 
-    /// constructors for ptr types
     template<class T>
-    StructCompactor( const BasicVec<T> &inst, const String &name = "", int par_level = 0 ) : name( name ), pointed_type( 0 ), par_level( par_level ), int_type( 0 ) { ///< generic structure
-        pointed_type = NEW( StructCompactor, inst[ 0 ], name + '[' + char( 'i' + par_level ) + ']', par_level + 1 );
-        type << "BasicVecRef<" << pointed_type->type << " >";
+    static Item *new_Item( const BasicVec<T> &inst, const String &name, int par_level ) {
+        return NEW( ItemVec, inst, name, par_level + 1 );
     }
 
-    /// called from apply_bs
+    // constructor
     template<class T>
-    void apply( const String &name, const T &val ) { items << NEW( StructCompactor, val, name ); } ///< ptr
+    StructCompactor( const T &val ) {
+        item = new_Item( val, "", 0 );
+    }
 
     // methods
-    void make_decl( String &os, const String &sp = "" );
-    void make_defi( String &os, const String &sp = "" );
-    void make_file();
-    void set_type( const char *t ) { type = t; }
-    StructCompactor *base_str();
-    String needed_alignement() const;
-    bool is_POD() const;
+    void make_files();
 
 private:
-    void make_copy( String &os, const String &sp, const String &dst, const String &src, int par_lev );
-    void make_push( String &os, const String &sp, const String &dst, int par_lev = 0, const String &suf = "", const String &n = "n" );
+    // void make_copy( String &os, const String &sp, const String &dst, const String &src, int par_lev );
+    // void make_push( String &os, const String &sp, const String &dst, int par_lev = 0, const String &suf = "", const String &n = "n" );
 
-    BasicVec<StructCompactor *> items;
-    String type;
-    String name;
-    StructCompactor *pointed_type; ///< if ptr (e.g. if BasicVec<...>)
-    int par_level;
-    bool int_type; ///< internal type
+    Item *item;
 };
 
 
